d70dde43b244dfe4ccf0ce212b662b16f8c51f90
Cadu Martins <csmartins.personal@gmail.com>
2023-07-30 20:46:22 -0300

    rotate image for calibration

--- app/src/main/java/com/example/expenses/QRCodeAnalyzer.kt
+++ app/src/main/java/com/example/expenses/QRCodeAnalyzer.kt
@@ -1,11 +1,13 @@
 package com.example.expenses
 
 import android.graphics.ImageFormat
+import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.zxing.*
 import com.google.zxing.common.HybridBinarizer
 import java.lang.Exception
+import java.nio.ByteBuffer
 
 class QRCodeAnalyzer(
     private val onQrCodeScanned: (String) -> Unit,
@@ -17,24 +19,28 @@ class QRCodeAnalyzer(
     )
 
     override fun analyze(image: ImageProxy) {
-        if (image.format in supportedImageFormats) {
+        if (image.format in supportedImageFormats && image.planes.size == 3) {
 //            image.planes.first().buffer
-            val planes = image.planes
-            val plane = planes.first()
-            val buffer = plane.buffer
-            val bytes = ByteArray(buffer.capacity()).also { buffer.get(it) }
+//            val planes = image.planes
+//            val plane = planes.first()
+//            val buffer = plane.buffer
+//            val bytes = ByteArray(buffer.capacity()).also { buffer.get(it) }
 
-            val source = PlanarYUVLuminanceSource(
-                bytes,
-                image.width,
-                image.height,
+            val rotatedImage = RotatedImage(getLuminancePlaneData(image), image.width, image.height)
+            rotateImageArray(rotatedImage, image.imageInfo.rotationDegrees)
+
+            val planarYUVLuminanceSource = PlanarYUVLuminanceSource(
+                rotatedImage.byteArray,
+                rotatedImage.width,
+                rotatedImage.height,
                 0,
                 0,
-                image.width,
-                image.height,
+                rotatedImage.width,
+                rotatedImage.height,
                 false
             )
-            val binaryBmp = BinaryBitmap(HybridBinarizer(source))
+            val hybridBinarizer = HybridBinarizer(planarYUVLuminanceSource)
+            val binaryBmp = BinaryBitmap(hybridBinarizer)
             try {
                 val result = MultiFormatReader().apply {
                     setHints(
@@ -44,13 +50,80 @@ class QRCodeAnalyzer(
                             )
                         )
                     )
-                }.decode(binaryBmp)
+                }.decodeWithState(binaryBmp)
+                Log.d("Barcode:", result.text)
                 onQrCodeScanned(result.text)
+
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
+
                 image.close()
             }
         }
     }
+
+    // 90, 180. 270 rotation
+    private fun rotateImageArray(imageToRotate: RotatedImage, rotationDegrees: Int) {
+        if (rotationDegrees == 0) return // no rotation
+        if (rotationDegrees % 90 != 0) return // only 90 degree times rotations
+
+        val width = imageToRotate.width
+        val height = imageToRotate.height
+
+        val rotatedData = ByteArray(imageToRotate.byteArray.size)
+        for (y in 0 until height) { // we scan the array by rows
+            for (x in 0 until width) {
+                when (rotationDegrees) {
+                    90 -> rotatedData[x * height + height - y - 1] =
+                        imageToRotate.byteArray[x + y * width] // Fill from top-right toward left (CW)
+                    180 -> rotatedData[width * (height - y - 1) + width - x - 1] =
+                        imageToRotate.byteArray[x + y * width] // Fill from bottom-right toward up (CW)
+                    270 -> rotatedData[y + x * height] =
+                        imageToRotate.byteArray[y * width + width - x - 1] // The opposite (CCW) of 90 degrees
+                }
+            }
+        }
+
+        imageToRotate.byteArray = rotatedData
+
+        if (rotationDegrees != 180) {
+            imageToRotate.height = width
+            imageToRotate.width = height
+        }
+    }
+
+    /**
+     * IMPORTANT: There's a known issue with the combination of CameraX and Zxing in some phones,
+     * especially OnePlus phones, where zxing doesn't detect any QR/Barcodes at all.
+     *
+     * To resolve that issue, we're required to cleanup the image data with this fix method
+     * @see <a href="https://github.com/beemdevelopment/Aegis/commit/fb58c877d1b305b1c66db497880da5651dda78d7">Aegis Authenticator Github Commit</a>
+     *
+     * @param image imageProxy from camera analyzer
+     * @return cleaned image bytearray
+     */
+    private fun getLuminancePlaneData(image: ImageProxy): ByteArray {
+        val plane = image.planes[0]
+        val buf: ByteBuffer = plane.buffer
+        val data = ByteArray(buf.remaining())
+        buf.get(data)
+        buf.rewind()
+        val width = image.width
+        val height = image.height
+        val rowStride = plane.rowStride
+        val pixelStride = plane.pixelStride
+
+        // remove padding from the Y plane data
+        val cleanData = ByteArray(width * height)
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                cleanData[y * width + x] = data[y * rowStride + x * pixelStride]
+            }
+        }
+        return cleanData
+    }
 }
+
+
+private class RotatedImage(var byteArray: ByteArray, var width: Int, var height: Int)
